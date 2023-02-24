dd742045178e2621d12682f3fda4d05d95a9ae73
BarbodH <habibi.k.barbod@gmail.com>
2023-02-24 14:08:12 -0500

    fixed script to handle file names with spaces

--- gather_metadata.sh
+++ gather_metadata.sh
@@ -8,12 +8,12 @@ function create_thumbnail {
 	fullname=$(basename -- "$1")
 	name="${fullname%.*}"
 	extension="${fullname##*.}"
-	convert $1 -resize ${dim}x${dim} "$(dirname $1)/.thumbs/${name}-${dim}.${extension}"
+	convert "$1" -resize ${dim}x${dim} "$(dirname "$1")/.thumbs/${name}-${dim}.${extension}"
 }
 
 DIR=${1:-./}
 
-if ! [ -d "$DIR" ] ; then
+if ! [ -d $DIR ] ; then
     echo $1 is not a directory. >&2
     exit 1
 fi
@@ -23,14 +23,14 @@ if ! [[ -r $DIR && -x $DIR ]] ; then
     exit 1
 fi
 
-find $DIR -iregex ".*\.\(jpg\|jpeg\|png\|tif\|tiff\|bmp\|gif\)$" | grep -v ".thumbs" | \
+find $DIR -iregex ".*\.\(jpg\|jpeg\|png\|tif\|tiff\|bmp\|gif\)$" | grep -v ".thumbs" | grep -v ".metadata" | \
 while read image; do 
 	declare -i height
 	declare -i width
 	declare -i max_dim
 
-	height=$(identify -format '%h' $image)
-	width=$(identify -format '%w' $image)
+	height=$(identify -format '%h' "$image")
+	width=$(identify -format '%w' "$image")
 
 	# determine maximum dimension length
 	if [ "$height" -gt "$width" ]; then
@@ -40,27 +40,27 @@ while read image; do
 	fi
 
 	# create .thumbnail folder if necessary
-	if [ ! -d "$(dirname $image)/.thumbs" ]; then
+	if [ ! -d "$(dirname "$image")/.thumbs" ]; then
 		mkdir "$(dirname $image)/.thumbs"
 	fi
 
-	if [ ! -d "$(dirname $image)/.metadata" ]; then
-		mkdir "$(dirname $image)/.metadata"
+	if [ ! -d "$(dirname "$image")/.metadata" ]; then
+		mkdir "$(dirname "$image")/.metadata"
 	fi
 	
 	# create thumbnails based on maximum dimension size
 	if [ "$max_dim" -gt 128 ]; then
-		create_thumbnail $image 128
+		create_thumbnail "$image" 128
 	fi
 
 	if [ "$max_dim" -gt 256 ]; then
-		create_thumbnail $image 256
+		create_thumbnail "$image" 256
 	fi
 
 	if [ "$max_dim" -gt 512 ]; then
-		create_thumbnail $image 512
+		create_thumbnail "$image" 512
 	fi
 
 	# create metadata text file
-	identify -verbose $image >> "$(dirname $image)/.metadata/$(basename -- $image).txt"
+	identify -verbose "$image" >> "$(dirname "$image")/.metadata/$(basename -- "$image").txt"
 done
