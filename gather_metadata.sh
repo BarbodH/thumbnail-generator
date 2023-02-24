0fc02a7057d9d9488ce261e2481fef53d8e43b6c
farhan-a3 <farhan.a3hd@gmail.com>
2023-02-23 19:25:46 -0500

    find image files and loop

--- gather_metadata.sh
+++ gather_metadata.sh
@@ -11,3 +11,8 @@ if ! [[ -r $DIR && -x $DIR ]] ; then
     echo $1 is not readable or executable. >&2
     exit 1
 fi
+
+find $DIR -iregex ".*\.\(jpg\|jpeg\|png\|tif\|tiff\|bmp\|gif\)$" | grep -v ".thumbs" | \
+while read IMAGE ; do 
+    mkdir $IMAGE../.thumbs
+    mkdir $IMAGE../.metadata
